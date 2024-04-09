b60974a232c2a036d12c358a2cb42474cdcb4bfc
SSol01 <s.sol@outlook.co.th>
2024-04-10 03:18:08 +0700

    commit3

--- index.php
+++ index.php
@@ -7,15 +7,152 @@
         <link rel="stylesheet" href="ofindex.css">
     </head>
     <body>
+        <div style="display: flex; justify-content: flex-end"><a href="#">สมัครสมาชิก</a>&emsp;
+        <a href="#">เข้าสู่ระบบ</a></div>
         <center><img src="src/banner.jpg" style="width: auto; height: auto;"></center><br>
         <center><div><a href="#">สูตรยอดฮิต</a>&nbsp;&nbsp;&emsp;
-        <a href="#">สูตรมาใหม่</a></div></center><br>
+        <a href="#">สูตรมาใหม่</a></div></center><br><br>
         <div>
         <center><input type="textbox" size="12" placeholder="ค้นหาสูตรอาหาร" style="font-size:11pt;">&nbsp;
         <label for="">
         <input type="submit" value="ค้นหา" name="find"></center>
         </div>
 <div class="maindiv">
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
   <div class="card">
     <div class="container">
         <h4><b>John Doe</b></h4>
