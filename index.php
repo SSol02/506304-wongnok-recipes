649e1ad76da4b3d93bff8931dc18eb3e946c9f1e
SSol01 <s.sol@outlook.co.th>
2024-04-10 02:28:35 +0700

    onepage commit

--- index.php
+++ index.php
@@ -4,8 +4,45 @@
         <title>
             Wongnok : สูตรอาหารวงนอก
         </title>
+        <link rel="stylesheet" href="ofindex.css">
     </head>
     <body>
-        TEST
+        <center><img src="src/banner.jpg" style="width: auto; height: auto;"></center><br>
+        <center><div><a href="#">สูตรยอดฮิต</a>&nbsp;&nbsp;&emsp;
+        <a href="#">สูตรมาใหม่</a></div></center><br>
+        <div>
+        <center><input type="textbox" size="12" placeholder="ค้นหาสูตรอาหาร" style="font-size:11pt;">&nbsp;
+        <label for="">
+        <input type="submit" value="ค้นหา" name="find"></center>
+        </div>
+<div class="maindiv">
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
+  <div class="card">
+    <div class="container">
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4>
+        <h4><b>John Doe</b></h4> 
+        <p>Architect & Engineer</p> 
+    </div>
+  </div>
+</div>
     </body>
 </html>
