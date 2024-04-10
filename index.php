e0d7a3de210f210d59414f1f949c9efeb3568db7
SSol01 <s.sol@outlook.co.th>
2024-04-10 09:56:40 +0700

    log regis

--- index.php
+++ index.php
@@ -5,10 +5,46 @@
             Wongnok : สูตรอาหารวงนอก
         </title>
         <link rel="stylesheet" href="ofindex.css">
+        <link rel="stylesheet" href="regisform.css">
     </head>
     <body>
-        <div style="display: flex; justify-content: flex-end"><a href="#">สมัครสมาชิก</a>&emsp;
-        <a href="#">เข้าสู่ระบบ</a></div>
+    <!--Login-->
+    <button class="open-button" onclick="openForm()">เข้าสู่ระบบ</button>
+        <div class="form-popup" id="myForm">
+      <form action="/action_page.php" class="form-container">
+        <h2>เข้าสู่ระบบ</h2>
+
+        <label for="email"><b>Email</b></label>
+        <input type="text" placeholder="ใส่ Email ที่ลงทะเบียนไว้" name="email" required>
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="ใส่รหัสผ่านที่ลงทะเบียนไว้" name="psw" required>
+
+        <button type="submit" class="btn">เข้าสู่ระบบ</button>
+        <button type="button" class="btn cancel" onclick="closeForm()">ยกเลิก</button>
+      </form>
+     </div>
+    <!--Login-->
+    <!--Regis-->
+        <button class="open-button" style="margin: 0px 125px 0px 0px;"onclick="openFormregis()">สมัครสมาชิก</button>
+        <div class="form-popup" id="myFormregis">
+      <form action="/action_page.php" class="form-container">
+        <h2>สมัครสมาชิก</h2>
+
+        <label for="user"><b>ชื่อที่แสดง</b></label>
+        <input type="text" placeholder="ชื่อที่จะแสดงในเว็บ" name="user" required>
+
+        <label for="email"><b>Email</b></label>
+        <input type="text" placeholder="Email" name="email" required>
+
+        <label for="psw"><b>สร้างรหัสผ่านใหม่</b></label>
+        <input type="password" placeholder="รหัสผ่านที่ใช้สำหรับเว็บนี้เท่านี้" name="psw" required>
+
+        <button type="submit" class="btn">สมัคร</button>
+        <button type="button" class="btn cancel" onclick="closeFormregis()">ยกเลิก</button>
+      </form>
+     </div>
+    <!--Regis-->
         <center><img src="src/banner.jpg" style="width: auto; height: auto;"></center><br>
         <center><div><a href="#">สูตรยอดฮิต</a>&nbsp;&nbsp;&emsp;
         <a href="#">สูตรมาใหม่</a></div></center><br><br>
@@ -183,3 +219,20 @@
 </div>
     </body>
 </html>
+<script>
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function openFormregis() {
+  document.getElementById("myFormregis").style.display = "block";
+}
+
+function closeFormregis() {
+  document.getElementById("myFormregis").style.display = "none";
+}
+</script>
